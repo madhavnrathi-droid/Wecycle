d2d9af20af22d3e75469d789691330285d66c250
madhavnrathi-droid <madhav.n.rathi@gmail.com>
2025-09-25 09:26:05 +0530

    fix: update turbo option as per Next.js requirements

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { turbo: true },
+  experimental: { turbo: {} },
 }
 module.exports = nextConfig
